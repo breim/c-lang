dcb063e726f83cc2d5cfea397ef03ecc6a446182
Henrique Breim <henrique@breim.com.br>
2014-02-22 14:50:03 -0300

    Tricks em matrizes

---
+++ matrizes.c
@@ -0,0 +1,46 @@
+//Tricks em C
+// <=> navinha pow pow pew <=> ----O  <\=> "sphrishh"
+ 
+
+// Iniciando um vetor
+int main(){
+	int v[10];
+	int v[] = {0,1,2,3,4,5,6,7,8,9}
+	char x[40];
+}
+
+//Iniciando uma matriz
+int main(){
+	int m[2][3]; // A matriz sempre inicia na linha.. neste caso temos duas linhas e tres colunas
+	int m[][] = {{7,8,4},4,9,2}}; // Neste caso é matriz 3x3
+}
+
+
+//Pegando o maior valor do vetor
+int maior = v[0];
+for(i=1;i<10;i++)
+	if(v[0]>maior)
+		maior = v[i];
+	printf("%d",maior);
+
+//Exibir os elementos pares na tela
+for(i=0;i<10;i++)
+	if(v[i] %2 == 0)
+		printf("%d",v[i]);
+
+//Contar quantos números negativos existem no vetor
+int cont = 0;
+for(i=0;i<10;i++)
+	if(v[i] < 0)
+		cont++;
+	printf("%d",cont);
+
+// Preencher a matriz
+int main(){
+	int m[2][3],l,c;
+	for(l=0;l<2;l++)
+		for(c=0;c<3;c++)
+			scanf("%d",&m[l][c]);
+
+
+}
